785961e5fbcdcbe4a7a9b285c6a5ff5827b16e3b
Biyeniose <biyeniose@gmail.com>
2024-07-15 20:35:43 -0400

    dev2 quick change

--- src/pages/addTransferPage.py
+++ src/pages/addTransferPage.py
@@ -5,3 +5,6 @@ st.button("Try Supabase", type="secondary")
 # when you press enter it updates
 title = st.text_input("Function name", "insert_player.pyS")
 st.write("The current movie title is", title)
+
+
+st.write("Extra change from dev2", title)
